d42bb58eb5eea6a1d47a7cf6f19234057c54d8b5
yeonmitc <82494535+yeonmitc@users.noreply.github.com>
2024-03-18 07:33:15 +0900

    add step 04
    .

--- test/src/main/java/kr/boot/basic/config/SpringConfig.java
+++ test/src/main/java/kr/boot/basic/config/SpringConfig.java
@@ -1,15 +1,22 @@
 package kr.boot.basic.config;
 
 
+import kr.boot.basic.repository.JdbcMemberRepository;
 import kr.boot.basic.repository.MemberRepository;
 import kr.boot.basic.repository.MemoryMemberRepository;
 import kr.boot.basic.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService(MemberRepository memberRepository) {
         return new MemberService(memberRepository);
@@ -17,7 +24,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
