1decdf9def94139de5944186fbeea3b3f2702d69
yeonmitc <82494535+yeonmitc@users.noreply.github.com>
2024-03-18 08:29:35 +0900

    Create TimeTraceAop.java
    .

---
+++ test/src/main/java/kr/boot/basic/aop/TimeTraceAop.java
@@ -0,0 +1,26 @@
+package kr.boot.basic.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    //@Around("execution(* kr.boot.basic..*(..))")
+    @Around("within(kr.boot.basic.controller..*)")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
